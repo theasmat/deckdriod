f0d614c07c6d0e5874d1533b953665238013d50f
theasmat <theasmat@users.noreply.github.com>
2026-05-18 07:53:15 +0530

    Trigger fully automated release v0.1.7 with Homebrew auto-update

--- deckdriod.rb
+++ deckdriod.rb
@@ -1,7 +1,7 @@
 class Deckdriod < Formula
   desc "Professional Android Development Dashboard"
   homepage "https://github.com/theasmat/deckdriod"
-  version "0.1.6"
+  version "0.1.7"
 
   if OS.mac?
     if Hardware::CPU.arm?
